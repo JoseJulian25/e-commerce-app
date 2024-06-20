087146c47065c48980655d658b7b38d9f0befa87
JoseJulian25 <jose.julianm2505@gmail.com>
2024-06-20 14:51:22 -0400

    fix problems

--- services/order/src/main/java/org/microservices/order/clients/PaymentClient.java
+++ services/order/src/main/java/org/microservices/order/clients/PaymentClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "payment-service", url = "${spring.config.payment-url}")
+@FeignClient(name = "payment-service", url = "${application.config.payment-url}")
 public interface PaymentClient {
 
     @PostMapping
