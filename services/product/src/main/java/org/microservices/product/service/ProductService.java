c7ca3ac8d02c91cbf949e676ab3cd9ea316c95d6
JoseJulian25 <jose.julianm2505@gmail.com>
2024-08-21 16:50:45 -0400

    code refactoring

--- services/product/src/main/java/org/microservices/product/service/ProductService.java
+++ services/product/src/main/java/org/microservices/product/service/ProductService.java
@@ -10,6 +10,7 @@ import org.microservices.product.entities.Product;
 import org.microservices.product.exception.ProductPurchaseException;
 import org.microservices.product.mapper.ProductMapper;
 import org.microservices.product.repository.ProductRepository;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -29,48 +30,68 @@ public class ProductService {
     }
 
     public List<ProductPurchaseResponse> purchaseProducts(List<ProductPurchaseRequest> requests) throws ProductPurchaseException {
-        List<Integer> productIds = requests.stream()
-                .map(ProductPurchaseRequest::productId)
-                .toList();
-
-        List<Product> storedProducts = repository.findAllByIdInOrderById(productIds);
-
-        if (productIds.size() != storedProducts.size()) {
-            throw new ProductPurchaseException("One or more products does not exit");
-        }
-
-        List<ProductPurchaseRequest> storedRequest = requests.stream().sorted(Comparator.comparing(ProductPurchaseRequest::productId)).toList();
-
-        List<ProductPurchaseResponse> purchasedProduct = new ArrayList<>();
-
-        for(int i = 0; i < storedProducts.size(); i++){
-            Product product = storedProducts.get(i);
-            ProductPurchaseRequest productRequest = storedRequest.get(i);
-
-            if(product.getAvailableQuantity() < productRequest.quantity()){
-                throw new ProductPurchaseException("Insufficient stock quantity with ID: " + productRequest.productId());
-            }
-
-            int newAvailableQuantity = product.getAvailableQuantity() - productRequest.quantity();
-            product.setAvailableQuantity(newAvailableQuantity);
-            repository.save(product);
-
-            purchasedProduct.add(mapper.toProductPurchaseResponse(product, productRequest.quantity()));
-        }
-
-        return purchasedProduct;
-
-
+        List<Product> storedProducts = fetchStoredProducts(requests);
+        validateProductExistence(requests, storedProducts);
+        return processPurchaseRequests(requests, storedProducts);
     }
+
     public ProductResponse findById(Integer productId) {
         return repository.findById(productId)
                 .map(mapper::toProductResponse)
                 .orElseThrow(() -> new EntityNotFoundException("Product not found with the ID: " + productId));
     }
 
+
     public List<ProductResponse> findAll() {
         return repository.findAll().stream()
                 .map(mapper::toProductResponse)
                 .toList();
+    }
+
+    private List<Product> fetchStoredProducts(List<ProductPurchaseRequest> requests) {
+        List<Integer> productIds = requests.stream()
+                .map(ProductPurchaseRequest::productId)
+                .toList();
+        return repository.findAllByIdInOrderById(productIds);
+    }
+
+    private void validateProductExistence(List<ProductPurchaseRequest> requests, List<Product> storedProducts) throws ProductPurchaseException {
+        if (requests.size() != storedProducts.size()) {
+            throw new ProductPurchaseException("One or more products do not exist");
+        }
+    }
+
+    private List<ProductPurchaseResponse> processPurchaseRequests(List<ProductPurchaseRequest> requests, List<Product> storedProducts) throws ProductPurchaseException {
+        List<ProductPurchaseRequest> sortedRequests = requests.stream()
+                .sorted(Comparator.comparing(ProductPurchaseRequest::productId))
+                .toList();
+
+        List<ProductPurchaseResponse> purchasedProducts = new ArrayList<>();
+
+        for (int i = 0; i < storedProducts.size(); i++) {
+            Product product = storedProducts.get(i);
+
+            ProductPurchaseRequest productRequest = sortedRequests.get(i);
+
+            validateStockAvailability(product, productRequest);
+
+            updateProductStock(product, productRequest);
+
+            purchasedProducts.add(mapper.toProductPurchaseResponse(product, productRequest.quantity()));
+        }
+
+        return purchasedProducts;
+    }
+
+    private void validateStockAvailability(Product product, ProductPurchaseRequest request) throws ProductPurchaseException {
+        if (product.getAvailableQuantity() < request.quantity()) {
+            throw new ProductPurchaseException("Insufficient stock quantity with ID: " + request.productId());
+        }
+    }
+
+    private void updateProductStock(Product product, ProductPurchaseRequest request) {
+        int newAvailableQuantity = product.getAvailableQuantity() - request.quantity();
+        product.setAvailableQuantity(newAvailableQuantity);
+        repository.save(product);
     }
 }
